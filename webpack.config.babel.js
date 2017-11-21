f4f3a79d0abc64b03630f0613e3c3270d87b9337
Yotau Hong <crazymousethief@outlook.com>
2017-11-21 19:39:21 +0800

    🎉Initialized with koa2
    Use webpack support the esm feature.
    All commits use gitmoji👻, look this site https://gitmoji.carloscuesta.me/

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path'
+import manifest from './package.json'
+
+export default [
+  {
+    name: 'server',
+    entry: './index.mjs',
+    target: 'node',
+    output: {
+      path: path.resolve(process.cwd(), 'dist'),
+      filename: 'index.js',
+    },
+    externals: (() => {
+      return Object.keys(manifest.dependencies).reduce((acc, cur) => {
+        acc[cur] = 'commonjs ' + cur
+        return acc
+      }, {})
+    })(),
+    module: {
+      rules: [
+        {
+          test: /(\.mjs|\.js)$/,
+          use: { loader: 'babel-loader' },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.mjs'],
+    },
+  },
+]

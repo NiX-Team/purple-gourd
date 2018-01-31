99de8754a1adcda8380738bcce4f208026072bec
Yotau Hong <crazymousethief@outlook.com>
2018-02-01 02:00:47 +0800

    🔧 Extract CSS for production

--- config/webpack.config.client.js
+++ config/webpack.config.client.js
@@ -1,11 +1,46 @@
 import webpack from 'webpack'
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import { CLIENT_PATH, NODE_ENV, OUTPUT_PATH } from './env'
+
+const css_module =
+  NODE_ENV === 'production'
+    ? {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: { loader: 'style-loader', options: { hmr: false } },
+          use: [
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1, minimize: true, modules: true, sourceMap: true, camelCase: true },
+            },
+          ],
+        }),
+      }
+    : {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1, modules: true, sourceMap: true, camelCase: true } },
+        ],
+      }
+
+const less_module =
+  NODE_ENV === 'production'
+    ? {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'less-loader'] }),
+      }
+    : {
+        test: /\.less$/,
+        loader: ['style-loader', 'css-loader', 'less-loader'],
+      }
 
 const clientConfig = {
   name: 'client',
-  devtool: 'cheap-module-source-map',
+  bail: NODE_ENV === 'production',
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'cheap-module-source-map',
   context: CLIENT_PATH,
   target: 'web',
   entry: ['./src/index.js'],
@@ -13,8 +48,8 @@ const clientConfig = {
     path: path.resolve(process.cwd(), 'dist'),
     publicPath: '/',
     pathinfo: true,
-    filename: 'assets/js/bundle.js?[hash]',
-    chunkFilename: 'assets/js/[name].chunk.js',
+    filename: 'assets/js/bundle.[chunkhash:8].js',
+    chunkFilename: 'assets/js/bundle.[chunkhash:8].chunk.js',
   },
   resolve: {
     extensions: ['.js', '.json', '.jsx'],
@@ -30,54 +65,27 @@ const clientConfig = {
         enforce: 'pre',
         loader: 'eslint-loader',
         include: CLIENT_PATH,
-        options: {
-          ignore: false,
-          useEslintrc: true,
-        },
+        options: { ignore: false, useEslintrc: true },
       },
       {
         oneOf: [
           {
             test: /\.(png|jpe?g|gif|bmp|svg)$/,
             loader: 'url-loader',
-            options: {
-              limit: 8192,
-              name: 'assets/media/[name].[hash:8].[ext]',
-            },
+            options: { limit: 8192, name: 'assets/media/[name].[hash:8].[ext]' },
           },
           {
             test: /\.(js|jsx)$/,
             loader: 'babel-loader',
             include: CLIENT_PATH,
-            options: {
-              cacheDirectory: true,
-            },
+            options: { cacheDirectory: true },
           },
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: {
-                  importLoaders: 1,
-                  modules: true,
-                  sourceMap: true,
-                  camelCase: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.less$/,
-            loader: ['style-loader', 'css-loader', 'less-loader'],
-          },
+          css_module,
+          less_module,
           {
             exclude: [/\.js$/, /\.html$/, /\.json$/],
             loader: 'file-loader',
-            options: {
-              name: 'assets/media/[name].[hash:8].[ext]',
-            },
+            options: { name: 'assets/media/[name].[hash:8].[ext]' },
           },
         ],
       },
@@ -86,16 +94,13 @@ const clientConfig = {
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: process.env.NODE_ENV,
-    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV }),
     new HtmlWebpackPlugin({
       inject: true,
       template: path.resolve(CLIENT_PATH, 'public/index.html'),
       favicon: 'public/favicon.ico',
-      minify: {
+      minify: NODE_ENV === 'production' && {
         removeComments: true,
         collapseWhitespace: true,
         removeRedundantAttributes: true,
@@ -109,17 +114,23 @@ const clientConfig = {
       },
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  ].concat(
-    NODE_ENV === 'production'
-      ? [
-          new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false, comparisons: false },
-            mangle: { safari10: true },
-            output: { comments: false, ascii_only: true },
-          }),
-        ]
-      : [],
-  ),
+  ]
+    .concat(
+      NODE_ENV === 'production'
+        ? [
+            new webpack.optimize.UglifyJsPlugin({
+              compress: { warnings: false, comparisons: false },
+              mangle: { safari10: true },
+              output: { comments: false, ascii_only: true },
+              sourceMap: true,
+            }),
+          ]
+        : [],
+    )
+    .concat(
+      NODE_ENV === 'production' ? [new ExtractTextPlugin({ filename: 'assets/css/style.[contenthash:8].css' })] : [],
+    )
+    .concat(NODE_ENV === 'production' ? [] : [new webpack.HotModuleReplacementPlugin()]),
   devServer: {
     contentBase: OUTPUT_PATH,
     hot: true,

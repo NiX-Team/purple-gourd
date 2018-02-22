49ff79c76f8fe6a4e7179edf35cac89be19670b8
Yotau Hong <crazymousethief@outlook.com>
2018-02-22 23:07:03 +0800

    ⬆️ Upgrade mongoose to version 5

--- server/src/models/mongoose.js
+++ server/src/models/mongoose.js
@@ -4,8 +4,6 @@ import filter from './filterPlugin'
 mongoose.Promise = global.Promise
 mongoose.plugin(filter)
 
-const connection = mongoose.createConnection('mongodb://localhost/db', {
-  useMongoClient: true,
-})
+const connection = mongoose.createConnection('mongodb://localhost/db')
 
 export default connection

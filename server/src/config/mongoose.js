d683c3d3852253339517e0558026579ac1ee65c1
Yotau Hong <crazymousethief@outlook.com>
2018-03-23 00:03:42 +0800

    🏗 Distinguish between development and production
    Work with docker compose in production environment.

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -4,6 +4,12 @@ import filter from '~/lib/filterPlugin'
 mongoose.Promise = global.Promise
 mongoose.plugin(filter)
 
-const connection = mongoose.createConnection('mongodb://localhost/db')
+const connection = mongoose.createConnection(
+  process.env.NODE_ENV === 'production' ? 'mongodb://mongo/db' : 'mongodb://localhost/db',
+  {
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 1000,
+  },
+)
 
 export default connection
--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,3 +1,3 @@
 import Redis from 'ioredis'
 
-export default new Redis()
+export default new Redis(process.env.NODE_ENV === 'production' ? { host: 'redis' } : {})
